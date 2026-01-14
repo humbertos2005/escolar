14fac9aed7ae978e99b6067a30094f5ce5bc8f6b
Humberto Simão de Moura <149192769+humbertos2005@users.noreply.github.com>
2026-01-14 12:17:03 -0400

    Atualizações nos templates e estilos conforme orientação

--- escola/listar_campos.py
+++ escola/listar_campos.py
@@ -5,10 +5,10 @@ db_path = os.environ.get("DATABASE_FILE", "escola.db")
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
-cursor.execute("PRAGMA table_info(dados_escola);")
+cursor.execute("PRAGMA table_info(faltas_disciplinares);")
 colunas = cursor.fetchall()
 
-print("Campos da tabela 'dados_escola':")
+print("Campos da tabela 'faltas_disciplinares':")
 for coluna in colunas:
     print("-", coluna[1])
 
